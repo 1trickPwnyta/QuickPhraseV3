4e6eb8845b712aa19d010d5e8a84e73f5b0aa42c
Sarah Pace <sarahpace37@gmail.com>
2022-11-29 00:17:53 -0800

    Initial commit

---
+++ QuickPhraseV3/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickPhraseV3;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ QuickPhraseV3/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace QuickPhraseV3;
+
+public partial class MainPage : ContentPage
+{
+	Random random = new Random();
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnNextClicked(object sender, EventArgs e)
+	{
+		List<String> phrases = new List<String>()
+		{
+			"donut hole", 
+			"hot dog", 
+			"point of view"
+		};
+
+		PhraseLabel.Text = phrases[random.Next(phrases.Count)];
+		NextButton.Text = "Next";
+	}
+}
+
